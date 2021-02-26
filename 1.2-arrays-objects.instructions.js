b784a4eff80e68d25b439269bd8f9ebc89b6d87b
jackiejones3bb <jackiejones3bb@gmail.com>
2021-02-25 22:04:31 -0500

    completion of challenge 1.2

--- 1.2-arrays-objects.instructions.js
+++ 1.2-arrays-objects.instructions.js
@@ -7,3 +7,19 @@
 */
 
 //NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
+
+let favMovie = {
+    nameOfMovie: 'Top Gun',
+    runTime: '1hr 50 min',
+    characters: [
+        {name: 'Maverick', age: 24, items:[{itemOne: 'sunglasses'}, {itemTwo: 'motorcycle'}]},
+        {name: 'Goose', age: 28, items:[{itemOne: 'wife'}, {itemTwo: 'son'}]}
+],
+    genre: 'action, drama'
+}
+
+console.log(favMovie.nameOfMovie)
+console.log(favMovie.runTime)
+console.log(favMovie.characters)
+console.log(favMovie.characters[0].name)
+console.log(favMovie.characters[0].items[0].itemOne)
